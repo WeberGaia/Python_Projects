0b6e7d74aa88c128636f42255660f709a435c7b2
Weber Gaia <webergaia@id.uff.br>
2023-01-17 12:30:49 -0300

    Learnig about OOP

--- Python_OO/conta.py
+++ Python_OO/conta.py
@@ -1,4 +1,21 @@
-
 class Conta:
+    def __init__(self, numero, titular, saldo, limite):  # Função construtora
+        print("")
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
-    pass
+
+    def extrato(self):
+        print("O saldo da conta é R${}".format(self.__saldo))
+
+    def depositar(self, valor):
+        self.__saldo += valor
+
+    def sacar(self, valor):
+        self.__saldo -= valor
+
+    def transferencia(self, valor, destino):
+        self.sacar(valor)
+        destino.depositar(valor)
--- Python_OO/main.py
+++ Python_OO/main.py
@@ -1,5 +1,22 @@
 from conta import Conta
+from datas import Datas
 
-conta = Conta()
-print(Conta(),"\n")
-print(conta)
+conta = Conta(123, "Weber", 80.0, 1000.0)
+
+#print("O nome do Titular é: {}".format(conta.titular))
+
+#conta.extrato()
+#conta.depositar(1000.0)
+#conta.extrato()
+#conta.sacar(200.0)
+#conta.extrato()
+
+#d = Datas(17, 1, 2023)
+#d.formatada()
+
+conta2 = Conta(123, "Weber", 50.0, 1000.0)
+
+conta.transferencia(30, conta2)
+
+conta.extrato()
+conta2.extrato()
