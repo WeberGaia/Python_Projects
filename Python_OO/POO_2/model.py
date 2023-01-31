505358a20bfc3b587eef34cfc87220dc9006e158
Weber Gaia <webergaia@id.uff.br>
2023-01-31 17:35:09 -0300

    OOP Advanced

--- Python_OO/POO_2/model.py
+++ Python_OO/POO_2/model.py
@@ -70,24 +70,37 @@ class Programa:
     def nome(self, nome):
         self._nome = nome
 
+    def __str__(self):
+        return f'{self.nome} - Likes: {self.likes}'
+
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
+
+    def __str__(self):
+        return f'{self.nome} - Likes: {self.likes} - Duração: {self.duracao}min'
+
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def __str__(self):
+        return f'{programa.nome} - Likes: {programa.likes} - {self.temporadas}temp'
+
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
 vingadores.dar_likes()
 vingadores.dar_likes()
 vingadores.dar_likes()
-
 atlanta.dar_likes()
 atlanta.dar_likes()
 
-print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
-print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+lista_store = [vingadores, atlanta]
+
+for programa in lista_store:
+    print(programa)
--- Python_OO/POO_2/tests_examples.py
+++ Python_OO/POO_2/tests_examples.py
@@ -1,25 +1,31 @@
 class Programa:  # Classe mãe
     def __init__(self, nome, ano):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
-        self.__like = 0
+        self._like = 0
 
     @property
     def nome(self):
-        return self.__nome
+        return self._nome
 
     @property
     def likes(self):
-        return self.__like
+        return self._like
 
     def dar_likes(self):
-        self.__like += 1
+        self._like += 1
+
+    def __str__(self):
+        return f'{self._nome} -  {self.likes}'
 
 
 class Filme(Programa):  # Classe filha
-    def __init__(self, nome, ano, duracao):         # Construtor de inicialização da classe filme
-        super().__init__(nome, ano)                 # Herança da classe mãe que devolve os métodos
+    def __init__(self, nome, ano, duracao):  # Construtor de inicialização da classe filme
+        super().__init__(nome, ano)          # Herança da classe mãe que devolve os métodos
         self.duracao = duracao
+
+    def __str__(self):
+        return f'{self._nome} -  {self.duracao}min - {self.likes}'
 
 
 class Serie(Programa):
@@ -27,15 +33,19 @@ class Serie(Programa):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
-
+    def __str__(self):
+        return f'{self._nome} -  {self.temporadas} temp - {self.likes}'
 # ==============================================================================
-filme = Filme("hitch", 2012, 140)
+filme = Filme("hitch", 2012, 140)  # Objeto
+filme.dar_likes()                  # Método dar likes
 filme.dar_likes()
-filme.dar_likes()
-print(f'{filme.nome} - {filme.duracao}min - {filme.ano} - Likes: {filme.likes}')
 
 serie = Serie("lista negra", 2010, 10)
 serie.dar_likes()
 serie.dar_likes()
 serie.dar_likes()
-print(f'{serie.nome} - {serie.temporadas}temp - {serie.ano} - Likes: {serie.likes}')
+
+lista = [filme, serie]
+
+for programa in lista:
+    print(programa)
